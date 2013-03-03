72e3aceb5bb6796353229d17a57be2b025bc9690
Shahin <shamscse@gmail.com>
2013-03-03 13:23:21 +0600

    Update task

--- online/menu/adminmenu.php
+++ online/menu/adminmenu.php
@@ -1,11 +1,11 @@
 <aside id="sidebar" class="column" style="height: 87.7%;">
-<<<<<<< HEAD
     <form class="quick_search">
         <input type="text" value="Quick Search" onfocus="if(!this._haschanged){this.value=''};this._haschanged=true;">
     </form>
     <hr/>
     <h3>Content</h3>
     <ul class="toggle">
+        <li class="icn_add_project"><a href="?page=newproject">Add New Project</a></li>
         <li class="icn_new_article"><a href="?page=newtask">New Task</a></li>
         <li class="icn_edit_article"><a href="#">Edit Task</a></li>
         <li class="icn_categories"><a href="#">Running Tasks</a></li>
@@ -29,42 +29,4 @@
         <p><strong>Copyright &copy; 2013 HCL PRO</strong></p>
         <p><a href="http://www.howladergroup.com/corporation">Howlader Corporation Ltd</a></p>
     </footer>
-=======
-		<form class="quick_search">
-			<input type="text" value="Quick Search" onfocus="if(!this._haschanged){this.value=''};this._haschanged=true;">
-		</form>
-		<hr/>
-		<h3>Content</h3>
-		<ul class="toggle">
-			<li class="icn_new_article"><a href="#">New Task</a></li>
-			<li class="icn_edit_article"><a href="#">Edit Task</a></li>
-			<li class="icn_categories"><a href="#">Running Tasks</a></li>
-			<li class="icn_tags"><a href="#">Completed Tasks</a></li>
-		</ul>
-		<h3>Users</h3>
-		<ul class="toggle">
-			<li class="icn_add_user"><a href="?page=newuser">Add New User</a></li>
-			<li class="icn_view_users"><a href="?page=showallusers">View Users</a></li>
-			<li class="icn_profile"><a href="#">Your Profile</a></li>
-		</ul>	
-                
-                <h3>Projects</h3>
-		<ul class="toggle">
-			<li class="icn_add_project"><a href="?page=newproject">Add New Project</a></li>
-			<li class="icn_add_project"><a href="#">View Projects</a></li>			
-		</ul>
-                
-		<h3>Admin</h3>
-		<ul class="toggle">
-			<li class="icn_settings"><a href="#">Options</a></li>
-			<li class="icn_security"><a href="#">Security</a></li>
-			<li class="icn_jump_back"><a href="logout.php">Logout</a></li>
-		</ul>
-		
-		<footer>
-			<hr />
-			<p><strong>Copyright &copy; 2013 HCL PRO</strong></p>
-			<p><a href="http://www.howladergroup.com/corporation">Howlader Corporation Ltd</a></p>
-		</footer>
->>>>>>> Shakil
 </aside><!-- end of sidebar -->
